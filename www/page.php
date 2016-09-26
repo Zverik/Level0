246a59e32dc0de385a712c11bd5456dac4b8dc5a
Ilya Zverev <zverik@textual.ru>
2016-09-26 15:02:24 +0200

    Update Leaflet CDN Links

--- www/page.php
+++ www/page.php
@@ -4,8 +4,8 @@
 <title><?=_('Level0 OpenStreetMap Editor') ?></title>
 <meta charset="utf-8">
 <meta name="generator" content="<?=GENERATOR ?>">
-<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
-<script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@0.7.7/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@0.7.7/dist/leaflet.js"></script>
 <style>body { font-family: sans-serif; font-size: 11pt; }</style>
 </head>
 <body>
